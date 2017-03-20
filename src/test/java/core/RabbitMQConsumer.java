be9f698ad3e169ce5980fcd9bd57db624c06bf0b
bug1024 <bug1024.com@gmail.com>
2017-03-20 23:02:53 +0800

    add elasticsearch rest client

--- src/test/java/core/RabbitMQConsumer.java
+++ src/test/java/core/RabbitMQConsumer.java
@@ -4,8 +4,13 @@ import com.rabbitmq.client.AMQP;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.DefaultConsumer;
 import com.rabbitmq.client.Envelope;
+import org.apache.http.HttpHost;
+import org.apache.http.util.EntityUtils;
+import org.elasticsearch.client.Response;
+import org.elasticsearch.client.RestClient;
 
 import java.io.IOException;
+import java.util.Collections;
 
 /**
  * RabbitMQ消费者
@@ -16,7 +21,12 @@ import java.io.IOException;
 public class RabbitMQConsumer {
 
     public static void main(String[] args) {
+        testES();
+    }
+
+    public static void testMQ() {
         final Channel channel = RabbitMQClient.INSTANCE.getChannel();
+
         try {
             Boolean autoAck = false;
             channel.basicConsume("q-canal", autoAck, "myConsumerTag", new DefaultConsumer(channel) {
@@ -34,6 +44,19 @@ public class RabbitMQConsumer {
                     }
                 }
             });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void testES() {
+        try {
+            RestClient restClient = RestClient.builder(new HttpHost("localhost", 9200, "http")).build();
+            Response response = restClient.performRequest("GET", "/",
+                    Collections.singletonMap("pretty", "true"));
+
+            System.out.println(EntityUtils.toString(response.getEntity()));
+
         } catch (Exception e) {
             e.printStackTrace();
         }
